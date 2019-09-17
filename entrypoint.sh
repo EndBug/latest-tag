ed9acb6be9706539ba9b5351b2466fb92e52be48
Federico Grandi <fgrandi30@gmail.com>
2019-09-17 22:25:27 +0200

    Add Docker files

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eu
+
+# Set up .netrc file with GitHub credentials
+git_setup ( ) {
+  cat <<- EOF > $HOME/.netrc
+        machine github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+
+        machine api.github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+EOF
+    chmod 600 $HOME/.netrc
+
+    git config --global user.email "actions@github.com"
+    git config --global user.name "Add & Commit GitHub Action"
+}
+
+"${GITHUB_REF:11}"
