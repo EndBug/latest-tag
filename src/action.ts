de650cf8c57f7a51fad1adccfc9b1b60a7bcc312
Federico Grandi <fgrandi30@gmail.com>
2020-03-09 12:56:45 +0100

    Refactoring

--- src/action.ts
+++ src/action.ts
@@ -1,9 +1,79 @@
 import { setFailed, getInput, info, warning } from '@actions/core'
 import { context, GitHub } from '@actions/github'
+const { stringify: str } = JSON
+
+const { GITHUB_ACTOR, GITHUB_SHA, GITHUB_TOKEN } = process.env
+type git = GitHub['git']
 
 function isRelease() {
   return context.payload.action == 'published'
     && context.payload.release?.tag_name != null
+}
+
+async function createRef(git: git) {
+  info(`Creating ref to point to ${GITHUB_SHA}...`)
+  let created = await git.createRef({
+    ...context.repo,
+    ref: 'refs/tags/latest',
+    sha: GITHUB_SHA
+  })
+  info('Ref created: ' + str(created.data))
+  return created
+}
+
+async function createTag(git: git, message: string) {
+  info(`Creating tag object for ${GITHUB_SHA}...`)
+  let tag = await git.createTag({
+    ...context.repo,
+    tag: 'latest',
+    message,
+    object: GITHUB_SHA,
+    type: 'commit',
+    tagger: {
+      name: GITHUB_ACTOR,
+      email: `${GITHUB_ACTOR}@users.noreply.github.com`,
+      date: (new Date()).toString()
+    }
+  })
+  info('Tag created: ' + str(tag.data))
+  return tag
+}
+
+async function deleteRef(git: git) {
+  info('Deleting ref...')
+  let deleted = await git.deleteRef({
+    ...context.repo,
+    ref: 'tags/latest'
+  })
+  info('Ref deleted')
+  return deleted
+}
+
+async function updateRef(git: git) {
+  info(`Updating ref to point to ${GITHUB_SHA}...`)
+  let updated = await git.updateRef({
+    ...context.repo,
+    force: true,
+    ref: 'tags/latest',
+    sha: GITHUB_SHA
+  })
+  info('Ref updated: ' + str(updated.data))
+  return updated
+}
+
+async function annotatedTag(git: git, message: string, matching: boolean) {
+  if (matching) await deleteRef(git)
+  let tag = await createTag(git, message)
+  if (tag.status != 201) {
+    warning('Creating tag obj resulted in an error:\n' + str(tag.data))
+    warning('The action will proceed in creating/updating a lightweight tag.')
+  }
+  return await createRef(git)
+}
+
+function lightweightTag(git: git, matching: boolean) {
+  if (matching) return updateRef(git)
+  else return createRef(git)
 }
 
 async function run() {
@@ -14,7 +84,6 @@ async function run() {
       return
     }
 
-    const { GITHUB_ACTOR, GITHUB_SHA, GITHUB_TOKEN } = process.env
     if (GITHUB_TOKEN) {
       const { git } = new GitHub(GITHUB_TOKEN)
 
@@ -25,52 +94,8 @@ async function run() {
 
       const message = getInput('description')
 
-      let tagResult
-      if (message) {
-        tagResult = await git.createTag({
-          ...context.repo,
-          tag: 'latest',
-          message,
-          object: GITHUB_SHA,
-          type: 'commit',
-          tagger: {
-            name: GITHUB_ACTOR,
-            email: `${GITHUB_ACTOR}@users.noreply.github.com`
-          }
-        })
-
-        if (tagResult.status != 201) {
-          warning('Creating tag obj resulted in an error:\n' + JSON.stringify(tagResult.data))
-          warning('The action will proceed in creating/updating a lightweight tag.')
-        } else info('Tag object successfully created.')
-      }
-
-      let newRef
-      if (macthingRef && tagResult?.status != 201) {
-        info(`Updating 'latest' tag to release commit: ${GITHUB_SHA}`)
-        newRef = await git.updateRef({
-          ...context.repo,
-          force: true,
-          ref: 'tags/latest',
-          sha: GITHUB_SHA
-        })
-      } else {
-        if (tagResult?.status == 201 && macthingRef) {
-          info('Deleting previous ref...')
-          await git.deleteRef({
-            ...context.repo,
-            ref: 'tags/latest'
-          })
-        }
-        info(`Creating 'latest' tag for release commit: ${GITHUB_SHA}`)
-        newRef = await git.createRef({
-          ...context.repo,
-          ref: 'refs/tags/latest',
-          sha: GITHUB_SHA
-        })
-      }
-
-      info('New ref available:\n' + JSON.stringify(newRef.data))
+      if (message) annotatedTag(git, message, !!macthingRef)
+      else lightweightTag(git, !!macthingRef)
     } else setFailed('Missing `GITHUB_TOKEN` environment variable')
   } catch (error) {
     setFailed(error instanceof Error ? error.message : error)
